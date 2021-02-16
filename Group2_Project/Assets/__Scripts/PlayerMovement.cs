f428e189ca797d258107da565124a47828c990a2
pmoeini1 <pmoeini@uwo.ca>
2021-02-16 18:09:14 -0500

    Adjusting scripts

--- Group2_Project/Assets/__Scripts/PlayerMovement.cs
+++ Group2_Project/Assets/__Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed = 0.5f;
-    float thrust = 2f;
+    float speed = 0.9f;
+    float thrust = 10f;
     public Rigidbody rb;
     Vector3 jump = new Vector3(0.0f, 2.0f, 0.0f);
     // Update is called once per frame
