d695b05d0d03b61f24a7cc777caf34354cd006ab
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-03-11 18:13:59 -0500

    Revert "Added Comments for some scripts  + More texture"
    This reverts commit 57fa4d9006f64115bb9d07fe12700237cfc123ca.

--- Rpg Project/Assets/_Scripts/StickToPlatform.cs
+++ Rpg Project/Assets/_Scripts/StickToPlatform.cs
@@ -2,14 +2,13 @@
 using System.Collections;
 　　
 　　public class StickToPlatform : MonoBehaviour {
-
-        //make sure that the player sticks to platform when it lands on it 
+　　
 　　	void OnTriggerStay(Collider other){
 　　		if (other.tag == "Platform") {
 　　			this.transform.parent = other.transform;
 　　		}
 　　	}
-        //make sure that the platform and player have seperate tags
+　　
 　　	void OnTriggerExit(Collider other){
 　　		if (other.tag == "Platform") {
 　　			this.transform.parent = null;
