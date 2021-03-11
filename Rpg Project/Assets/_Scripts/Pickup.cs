e83d97f77da403d92bc5b0611131d766e52de2c0
pmoeini1 <pmoeini@uwo.ca>
2021-03-11 18:58:34 -0500

    Can pickup health cubes

---
+++ Rpg Project/Assets/_Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour, IPickup
+{
+    void OnTriggerEnter(Collider col){
+        if (col.gameObject.tag == "Player"){
+            Disappear();
+        }
+        
+    }
+
+    public void Disappear(){
+        print("Pickup hit");
+        Destroy(this.gameObject);
+    }
+}
