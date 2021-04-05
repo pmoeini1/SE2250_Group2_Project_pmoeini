50e490134e82e49ac27843800c4ff4d9b4b33465
HerbertShin <hshin63@uwo.ca>
2021-04-05 19:12:13 -0400

    Added script to character
    Character now animates

--- Rpg Project/Assets/__Scripts/sprite.cs
+++ Rpg Project/Assets/__Scripts/sprite.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class sprite : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
+    bool jump;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,17 @@ public class sprite : MonoBehaviour
     {
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        transform.position = transform.position + horizontal * speed * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump")){
+            animator.SetBool("Jump",true);
+            jump = true;
+        }
+
+        else if (Input.GetButtonUp("Jump")){
+            animator.SetBool("Jump", false);
+            jump=false;
+        }
         
     }
 }
